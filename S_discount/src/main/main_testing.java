7581ae2e99491e6f0f9f47e6b4b09e6f9bf45998
cartrabbit <cartrabbit@cartrabbit>
2021-09-24 13:09:34 +0530

    Retain

--- S_discount/src/main/main_testing.java
+++ S_discount/src/main/main_testing.java
@@ -28,7 +28,6 @@ import title.title;
 
 public class main_testing {
 	WebDriver driver;
-	
 
 	@BeforeTest
 	public void setup() throws InterruptedException {
@@ -39,31 +38,12 @@ public class main_testing {
 		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 		driver.get("http://localhost/wordpress/wp-login.php");
 	}
-		
-		
-		@Test
-		public void login() throws InterruptedException, AWTException {
-			
-		choose_discount_type Choosediscounttype=PageFactory.initElements(driver, choose_discount_type.class);
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+
+	@Test
+	public void login() throws InterruptedException, AWTException {
+
+		choose_discount_type Choosediscounttype = PageFactory.initElements(driver, choose_discount_type.class);
+
 		wordpressloginpage.username(driver).sendKeys("user@example.com");
 		wordpressloginpage.password(driver).sendKeys("W");
 		wordpressloginpage.loginbutton(driver).click();
@@ -79,59 +59,14 @@ public class main_testing {
 		filterby.list_notlist("in_list");
 		filterby.Enter_product_name(driver).sendKeys("Toys");
 		robot.robot(null);
-//		filterby.d(driver).click();
-//		filterby.Enter_filter_type("product_category");
-//		filterby.list_notlist("in_list");
-//		filterby.Enter_product_name(driver).sendKeys("Uncategorized");
-//		robot.robot(null);
+
 		PageFactory.initElements(driver, discounts.class);
-discounts.discounttype("fixed_price");
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-			
-		
-		
-		
-		
-		
+		discounts.discounttype("fixed_price");
 		
 		
 		
 
-//		driver.findElement(By.id("user_login")).sendKeys("user@example.com");
-//		driver.findElement(By.id("user_pass")).sendKeys("W");
-//		driver.findElement(By.id("wp-submit")).click();
-//		Actions action = new Actions(driver);
-//		WebElement element = driver.findElement(By.xpath("//*[@id=\"toplevel_page_woocommerce\"]/a/div[3]"));
-//		action.moveToElement(element).build().perform();
-//		Thread.sleep(2000);
-//		WebElement addproduct = driver.findElement(By.xpath("//*[@id=\"toplevel_page_woocommerce\"]/ul/li[4]/a"));
-//		addproduct.click();
-//		driver.findElement(By.xpath("//*[@id=\"wpbody-content\"]/div/div/a[1]")).click();
-//	  
-//		
-//		
+
 
 	}
 
